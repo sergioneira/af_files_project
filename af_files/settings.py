b715cda61684b5fdcfe0c07839611e6506bb9656
Sergio Neira <sneira@techk.cl>
2020-08-03 22:02:37 -0400

    test urls

--- af_files/settings.py
+++ af_files/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'af_files.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'af_files_db',
     }
 }
