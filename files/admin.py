fef5472ee0e63bea3ff0b0b27e62813d77cbf70a
Sergio Neira <sneira@techk.cl>
2020-08-05 22:46:39 -0400

    add models to admin

--- files/admin.py
+++ files/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import (
+    Client,
+    Institution,
+    HealthPlan,
+)
 
-# Register your models here.
+
+class ClientAdmin(admin.ModelAdmin):
+    pass
+
+
+class InstitutionAdmin(admin.ModelAdmin):
+    pass
+
+
+class HealthPlanAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Institution, InstitutionAdmin)
+admin.site.register(HealthPlan, HealthPlanAdmin)
