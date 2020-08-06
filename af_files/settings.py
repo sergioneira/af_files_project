5b3ebc9dcff45f536fbe5eeeee718ee33da40e1a
Sergio Neira <sneira@techk.cl>
2020-08-05 22:39:23 -0400

    postgres

--- af_files/settings.py
+++ af_files/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'af_files.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'af_files_db',
     }
 }
